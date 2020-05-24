dfb58c886fc231526a6dee1c8c245a01f8bb0e0e
skillbasis <65834267+skillbasis@users.noreply.github.com>
2020-05-23 19:31:07 -0500

    Add files via upload

---
+++ pka-common.js
@@ -0,0 +1,7 @@
+function load_cabecalho_pka(){
+	document.write('<img src="https://skillbasis.github.io/logo-skillbasis-pt.gif" height="108px">');
+}//end function
+//load_cabecalho_pka();
+function load_rodape_pka(){
+	document.write('<img src="https://skillbasis.github.io/logo-skillbasis-pt.gif" height="108px">');
+}//end function
